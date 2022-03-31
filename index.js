89b67ce3276208d7c78de16c6d26d1daf9b64d27
URIEL EIAL ROZIC <46025935@ort>
2022-03-31 12:12:22 -0300

    progreso

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+/*const http = require("http");
 
 const host = 'localhost';
 const port = 8000;
@@ -13,4 +13,18 @@ const server = http.createServer(requestListener);
 
 server.listen(port, host, () =>{
     console.log(`server is running on http://${host}:${port}`);
-});
+});*/
+
+var lista = [11,33,2,-1,110,99,8];
+let numMayor = 0;
+let segundoMayor = 0;
+
+for(let i = 0;i< lista.length;i++){
+    let e = lista[i];
+    if(e % 2 != 0 && e > numMayor){
+        numMayor = e;
+    }else if(e % 2 != 0 && e < numMayor && e > segundoMayor){
+        segundoMayor = e;
+    }
+    console.log(segundoMayor);
+}
